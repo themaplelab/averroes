3deb808808aefb7d73d6ec29538b459bd08f03df
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2014-02-26 22:21:40 +0000

    unrollme now has its placeholder library
    Former-commit-id: 569ce42cc437fab53780c87692134a5b16f3c1e7

--- src/ca/uwaterloo/averroes/jar/AndroidJarFactory.java
+++ src/ca/uwaterloo/averroes/jar/AndroidJarFactory.java
@@ -54,8 +54,6 @@ public class AndroidJarFactory {
 			Options.v().set_force_android_jar(AverroesProperties.defaultAndroidJar());
 			Options.v().set_validate(true);
 
-			System.out.println(AverroesProperties.getAndroidAppClassPath());
-
 			// Load the necessary classes
 			TimeUtils.reset();
 			System.out.println("");
@@ -67,10 +65,6 @@ public class AndroidJarFactory {
 			// Print some statistics
 			System.out.println("# application classes: " + Scene.v().getApplicationClasses().size());
 			System.out.println("# library classes: " + Scene.v().getLibraryClasses().size());
-
-			// String className = "com.flurry.sdk.kg$a";
-			// System.out.println(Scene.v().getSootClass(className).getFields());
-			// System.exit(0);
 
 			// Now let Averroes do its thing
 			// First, create the class hierarchy
