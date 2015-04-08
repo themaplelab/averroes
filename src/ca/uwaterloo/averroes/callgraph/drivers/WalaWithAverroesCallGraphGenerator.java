99c10d80c21dfe00933576835c817032cb25caa4
karim <karim@e628a048-d915-0410-9aa0-f7087ab87138>
2015-04-08 15:00:40 +0000

    adding drivers for wala and wala-averroes
    Former-commit-id: 617a706d0b952aef300ad4ef19df228d5f9b71a6

---
+++ src/ca/uwaterloo/averroes/callgraph/drivers/WalaWithAverroesCallGraphGenerator.java
@@ -0,0 +1,51 @@
+package ca.uwaterloo.averroes.callgraph.drivers;
+
+import ca.uwaterloo.averroes.callgraph.CallGraph;
+import ca.uwaterloo.averroes.callgraph.CallGraphFactory;
+import ca.uwaterloo.averroes.callgraph.gxl.GXLWriter;
+import ca.uwaterloo.averroes.exceptions.AverroesException;
+import ca.uwaterloo.averroes.properties.AverroesProperties;
+import ca.uwaterloo.averroes.util.TimeUtils;
+import ca.uwaterloo.averroes.util.io.FileUtils;
+
+/**
+ * A driver class that generates call graph for WALA using the averroes placeholder library.
+ * 
+ * @author karim
+ * 
+ */
+public class WalaWithAverroesCallGraphGenerator {
+
+	public static void main(String[] args) {
+		try {
+			// Generate the call graph
+			TimeUtils.reset();
+			if (args.length != 1) {
+				usage();
+				throw new AverroesException("WalaAverroes expects exactly 1 argument.");
+			}
+
+			// Process the arguments
+			String benchmark = args[0];
+			
+			FileUtils.createDirectory(AverroesProperties.getOutputDir());
+			CallGraph wala = CallGraphFactory.generateWalaWithAverroesCallGraph(benchmark);
+			System.out.println("Total time to finish: " + TimeUtils.elapsedTime());
+			new GXLWriter().write(wala, FileUtils.walaAverroesCallGraphFile());
+
+			// Print some statistics
+			System.out.println("=================================================");
+			System.out.println("# edges = " + wala.size());
+			System.out.println("=================================================");
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	public static void usage() {
+		System.out.println("");
+		System.out.println("Usage: java -jar wala-averroes.jar");
+		System.out.println("");
+	}
+}
