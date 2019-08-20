7446b4e4ef974337b16bc43403259f70edbfc6ec
Karim Ali <karim.ali@ualberta.ca>
2019-08-20 10:22:34 -0600

    reorganize packages for privateclasses

---
+++ src/test/java/averroes/testsuite/privateclasses/input/A.java
@@ -0,0 +1,50 @@
+package averroes.testsuite.privateclasses.input;
+
+import java.util.Iterator;
+
+/**
+ * This example uses a private inner class to implement
+ * an iterator.  
+ *
+ */
+public class A {
+	
+	private static class It implements Iterator {
+
+		It(Object o){
+			obj = o;
+			done = false;
+		}
+		
+		@Override
+		public boolean hasNext() { 
+			return !done;
+		}
+
+		@Override
+		public Object next() { 
+			done = true;
+			return obj;
+		}
+		
+		private Object obj;
+		private boolean done;
+		
+	}
+	
+	public void put(Object k, Object v){
+		key = k;
+		value = v;
+	}
+	
+	public Iterator keys() {
+		return new It(key);
+	}
+	
+	public Iterator values() {
+		return new It(value);
+	}
+	
+	private Object key;
+	private Object value;
+}
---
+++ src/test/java/averroes/testsuite/privateclasses/output/xta/XTA.java
@@ -0,0 +1,5 @@
+package averroes.testsuite.privateclasses.output.xta;
+
+public class XTA {
+	public static boolean booleanValue;
+}
