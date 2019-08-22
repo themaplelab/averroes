797cfad27b7b1306bb04fe63e016733f2e80e95b
Karim Ali <karim.ali@ualberta.ca>
2019-08-22 10:54:01 -0600

    enable all test cases again

--- src/test/java/averroes/tests/AllTests.java
+++ src/test/java/averroes/tests/AllTests.java
@@ -1,6 +1,15 @@
 package averroes.tests;
 
 import averroes.tests.junit.TestAnonymousClasses;
+import averroes.tests.junit.TestArrays;
+import averroes.tests.junit.TestCasting;
+import averroes.tests.junit.TestExceptions;
+import averroes.tests.junit.TestInheritance;
+import averroes.tests.junit.TestLinkedList;
+import averroes.tests.junit.TestMultipleClasses;
+import averroes.tests.junit.TestNestedClasses;
+import averroes.tests.junit.TestPrivateClasses;
+import averroes.tests.junit.TestSimple;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -8,14 +17,14 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({
   TestAnonymousClasses.class,
-  //	TestArrays.class,
-  //	TestCasting.class,
-  //	TestExceptions.class,
-  //	TestInheritance.class,
-  //	TestLinkedList.class,
-  //	TestMultipleClasses.class,
-  //	TestNestedClasses.class,
-  //	TestPrivateClasses.class,
-  //	TestSimple.class
+  TestArrays.class,
+  TestCasting.class,
+  TestExceptions.class,
+  TestInheritance.class,
+  TestLinkedList.class,
+  TestMultipleClasses.class,
+  TestNestedClasses.class,
+  TestPrivateClasses.class,
+  TestSimple.class
 })
 public class AllTests {}
