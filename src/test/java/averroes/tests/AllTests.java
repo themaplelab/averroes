04c2535c8b78f475cd6bf22eb29d70b3a51e4525
David Seekatz <seekatz@ualberta.ca>
2020-05-27 10:52:50 -0600

    remove duplicate tests so gradle can run tests in parallel

--- src/test/java/averroes/tests/AllTests.java
+++ src/test/java/averroes/tests/AllTests.java
@@ -5,17 +5,17 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
-@SuiteClasses({
-  TestAnonymousClasses.class,
-  TestArrays.class,
-  TestCasting.class,
-  TestExceptions.class,
-  TestInheritance.class,
-  TestLinkedList.class,
-  TestMultipleClasses.class,
-  TestNestedClasses.class,
-  TestPrivateClasses.class,
-  TestSimple.class
-})
+//@RunWith(Suite.class)
+//@SuiteClasses({
+//  TestAnonymousClasses.class,
+//  TestArrays.class,
+//  TestCasting.class,
+//  TestExceptions.class,
+//  TestInheritance.class,
+//  TestLinkedList.class,
+//  TestMultipleClasses.class,
+//  TestNestedClasses.class,
+//  TestPrivateClasses.class,
+//  TestSimple.class
+//})
 public class AllTests {}
